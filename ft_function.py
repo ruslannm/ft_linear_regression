8cbfa32f033d811704e9496a78727c66126ad84d
ruslannm <brak@mail.ru>
2021-04-29 20:39:15 +0300

    2

--- ft_function.py
+++ ft_function.py
@@ -1,5 +1,32 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_function.py                                     :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rgero <marvin@42.fr>                       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/04/29 20:02:55 by rgero             #+#    #+#              #
+#    Updated: 2021/04/29 20:20:28 by rgero            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#from os import path
+import pandas as pd
+
+def get_cfg(filename):
+	if not path.exists(filename):
+        print(f'\nError: config file "{filename}" not found!')
+        exit(1)
+
+	df = pd.read_csv('ft_linear_regression.cfg')
+	print(df.head())
+	
+
+
 def estimateprice(teta0, teta1, mileage):
 	return teta0 + teta1 * mileage
 
-def get_param():
-		
+#def get_param():
+
+if __name__ == "__main__":
+	get_cfg()
--- predict.py
+++ predict.py
@@ -1,4 +1,32 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    predict.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rgero <marvin@42.fr>                       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/04/29 20:15:26 by rgero             #+#    #+#              #
+#    Updated: 2021/04/29 20:37:51 by rgero            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from os import path
 import pandas as pd
 
-df = pd.read_csv("data.csv")
-print(df.head(5))
+def read_params(filename):
+
+    theta = [0, 0]
+    if path.exists(filename):
+        df = pd.read_csv(filename, header = None)
+        print(len(df.columns))
+#        for y in agg.columns:
+#            if(df[y].dtype == np.float64 or agg[y].dtype == np.int64):
+#          treat_numeric(agg[y])
+#    else:
+#          treat_str(agg[y])
+
+        print(df.head())
+    return theta
+
+if __name__ == "__main__":
+    read_params('params.csv')
