d3ac9b8c9d30aab9f22e4ca892797cc6aac15fd0
ruslannm <brak@mail.ru>
2021-04-29 22:03:58 +0300

    predict

--- ft_function.py
+++ ft_function.py
@@ -6,27 +6,44 @@
 #    By: rgero <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/29 20:02:55 by rgero             #+#    #+#              #
-#    Updated: 2021/04/29 20:20:28 by rgero            ###   ########.fr        #
+#    Updated: 2021/04/29 21:51:02 by rgero            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#from os import path
+from os import path
+import numpy as np
 import pandas as pd
 
-def get_cfg(filename):
-	if not path.exists(filename):
-        print(f'\nError: config file "{filename}" not found!')
+def ft_read_csv(filename, columns):
+
+    is_correct = 0
+    if path.exists(filename):
+        df = pd.read_csv(filename)
+        if list(df.columns) == columns:
+            for y in df.columns:
+                if(df[y].dtype == np.float64 or df[y].dtype == np.int64):
+                    is_correct += 1
+    if is_correct == len(columns):                
+        return df
+    else:
+        print('\nError: The content of the file {} doesn\'t respect the norme'\
+              .format(filename))
         exit(1)
 
-	df = pd.read_csv('ft_linear_regression.cfg')
-	print(df.head())
-	
+def ft_is_number(str, only_positive, str_name):
+    try:
+        float(str)
+        if only_positive and float(str) < 0:
+            print('\nError: {} must be only positive'.format(str_name))
+            return False
+        return True
+    except ValueError:
+        print('\nError: {} must be number'.format(str_name))
+        return False
 
+def estimateprice(theta, mileage):
+    return theta[0] + theta[1] * mileage
 
-def estimateprice(teta0, teta1, mileage):
-	return teta0 + teta1 * mileage
-
-#def get_param():
 
 if __name__ == "__main__":
-	get_cfg()
+    ft_read_csv('params.csv', ['theta0', 'theta1'])
--- predict.py
+++ predict.py
@@ -6,27 +6,26 @@
 #    By: rgero <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/29 20:15:26 by rgero             #+#    #+#              #
-#    Updated: 2021/04/29 20:37:51 by rgero            ###   ########.fr        #
+#    Updated: 2021/04/29 22:02:33 by rgero            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from os import path
-import pandas as pd
+import ft_function as ft
 
-def read_params(filename):
+def predict():
 
-    theta = [0, 0]
-    if path.exists(filename):
-        df = pd.read_csv(filename, header = None)
-        print(len(df.columns))
-#        for y in agg.columns:
-#            if(df[y].dtype == np.float64 or agg[y].dtype == np.int64):
-#          treat_numeric(agg[y])
-#    else:
-#          treat_str(agg[y])
-
-        print(df.head())
-    return theta
+    user_input = input('Please enter a mileage: ')
+    if ft.ft_is_number(user_input, 1, 'mileage'):
+        mileage = float(user_input)
+    else:
+        exit(1)
+    theta = (0, 0)
+    if path.exists('params.csv'):
+        df = ft.ft_read_csv('params.csv', ['theta0', 'theta1'])
+        theta = (df.iloc[0]['theta0'], df.iloc[0]['theta1'])
+    estimate_price = ft.estimateprice(theta, mileage)
+    print(f'This car costs {round(estimate_price)}')
 
 if __name__ == "__main__":
-    read_params('params.csv')
+    predict()
